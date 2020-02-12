3edb250798e331a7d719fd45188306a848161492
nguyenhongthai2403 <nguyenhongthai2403@gmail.com>
2020-02-12 09:25:44 +0700

    Hello

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+
+var hostname = "localhost";
+var port = "2403";
+
+app.get("/", (req, res )=>{
+    res.send("<h1>Hello world</h1>");
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Listening on ${hostname}: ${port}`);
+});
